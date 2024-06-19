1f5b6a21fabc164dac55dc2e6adbeeb6454a4656
SettleScores <settle.scores.biz@gmail.com>
2024-06-19 17:03:31 +0300

    19.06.2024. Vlad. Test commit. qqq

--- src/util/generateToken.ts
+++ src/util/generateToken.ts
@@ -14,7 +14,7 @@ export const generateLogoutTokenFromToken = (accessToken: string) => {
   try {
     decoded = jwt.verify(accessToken, EnvVars.Jwt.Secret);
   } catch(err) {
-    /// err TODO normal err
+    /// TODO normal err
   }
 
   return generateTokenFromUserId(decoded.id, 0)
